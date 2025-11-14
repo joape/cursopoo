4b492a3572d7f702b3ed37fde6be6de14420587d
jpedrozo <joaquin.pedrozo@gmail.com>
2025-11-14 20:50:54 -0300

    agrego ejercicio en python

---
+++ python/clase.py
@@ -0,0 +1,17 @@
+edad_input = input("Introduce tu edad: ").strip()
+try:
+    edad = int(edad_input)
+    if edad < 0:
+        print("Edad inválida (negativa).")
+    elif edad < 13:
+        print("Eres un niño.")
+    elif edad <= 17:
+        print("Eres un adolescente.")
+    elif edad <= 64:
+        print("Eres un adulto.")
+    else:
+        print("Eres un adulto mayor.")
+except ValueError:
+    print("Edad inválida (debe ser un número entero).")
+
+edad_input # type: ignore
